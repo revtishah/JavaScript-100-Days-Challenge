cb4ed186d0ef75bc74c1041f074c83bbbcbdfd83
Revti Shah <revti.shah@bitscape.com>
2022-05-06 10:56:56 +0530

    Click to copy code has updated

--- Day_7_Click_To_Copy/script.js
+++ Day_7_Click_To_Copy/script.js
@@ -3,15 +3,14 @@ const coupon = document.querySelector(".coupon");
 
 const copyText = (e) => {
   e.preventDefault();
-  console.log("hello");
-  coupon.select();
-  coupon.setSelectionRange(0, coupon.value.length);
   // document.execCommand("copy");
-  navigator.clipboard.writeText(coupon.value);
-  btn.textContent = "Copied!!";
-  setTimeout(() => {
-    btn.textContent = "Copy";
-  }, 3000);
+  navigator.clipboard.writeText(coupon.value).then(() => {
+    btn.textContent = "Copied!!";
+    setTimeout(() => {
+      btn.textContent = "Copy";
+    }, 3000);
+  });
+
 };
 
 btn.addEventListener("click", copyText);
