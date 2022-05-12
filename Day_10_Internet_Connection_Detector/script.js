1a812344783b65fa999b9463ce31b17f9e032ac9
Revti Shah <revti.shah@bitscape.com>
2022-05-12 18:56:15 +0530

    Internet Connection Detector is in process

---
+++ Day_10_Internet_Connection_Detector/script.js
@@ -0,0 +1,21 @@
+//VARIABLES
+
+const image = document.getElementById("image");
+const statusDisplay = document.getElementById("status");
+const bgColor = document.getElementById("main");
+
+function setColor() {
+  bgColor.classList.add("online");
+}
+
+async function connectionStatus() {
+  try {
+    const fetchResult = await fetch(
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time=" +
+        new Date().getTime()
+    );
+    image.src = "./images/online.png";
+    setColor();
+    return fetchResult.status >= 200 && fetchResult.status < 300;
+  } catch {}
+}
